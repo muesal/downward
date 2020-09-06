1164e778a159957ae9ca4ebe6c2f66a8f97e82c8
salo1893 <salome.mueller@alansa.ch>
2020-09-06 14:31:39 +0200

    removed bug in get_mutex_with_state

--- src/search/potentials/mutex_based_potential_heuristics.cc
+++ src/search/potentials/mutex_based_potential_heuristics.cc
@@ -22,6 +22,7 @@
 //For the table:
 #include <iostream>
 #include <fstream>
+#include <zconf.h>
 
 using namespace std;
 using namespace hm_heuristic;
@@ -93,10 +94,14 @@ namespace potentials {
     static Tuple get_mutex_with_state(map<int, int> &state, vector<Tuple> &mutexes) {
         Tuple mf;
         for (Tuple mutex : mutexes) {
-            if (state[mutex[0].var] == mutex[0].value) {
-                mf.push_back(mutex[1]);
-            } else if (state[mutex[1].var] == mutex[1].value) {
-                mf.push_back(mutex[0]);
+            if (state.find(mutex[0].var) != state.end()) {
+                if (state.find(mutex[0].var)->second == mutex[0].value) {
+                    mf.push_back(mutex[1]);
+                }
+            } else if (state.find(mutex[1].var) != state.end()) {
+                if (state.find(mutex[1].var)->second == mutex[1].value) {
+                    mf.push_back(mutex[0]);
+                }
             }
         }
         return mf;
@@ -114,7 +119,6 @@ namespace potentials {
     get_all_extensions(map<int, int> &state, int k, int last_assigned, vector<vector<int>> &domains,
                        vector<map<int, int>> &extended) {
         if (k == 0) {
-            // TODO: does this realy save a copy of the state?
             extended.push_back(state);  // Add the state to the list, if it is fully extended.
             return extended;
         }
@@ -122,11 +126,12 @@ namespace potentials {
         // variables assigned from beginning to end, therefore enough unassigned variables need to remain free
         // size - k if only not assigned values left, size - k - 1 otherwise
         int before = state.begin()->second < last_assigned ? 0 : 1;
-        for (size_t i = last_assigned; i < domains.size() - k - before; i++) { //domains.size(), as there are exactly as many domains as variables in the state
+        for (size_t i = last_assigned; i < domains.size() - k -
+                                           before; i++) { //domains.size(), as there are exactly as many domains as variables in the state
             if (state.find(i) == state.end()) { // if the variable is unassigned
                 for (int d : domains[i]) {
                     state[i] = d;               // assign it to all domains and get all extensions
-                    get_all_extensions(state, k - 1, (int) i + 1, domains, extended); //TODO: does this change extended?
+                    get_all_extensions(state, k - 1, (int) i + 1, domains, extended);
                 }
                 state.erase(i);                 // unassign the variable
             }
@@ -149,48 +154,6 @@ namespace potentials {
     }
 
     /**
-     * TODO: The following probably have the first 11 lines in common... AND is single_fact needed? can it be used somewhere to simplify things, eg. et the beginning of mutli fact?
-    static bool single_fact_diasambiguation(State &state, VariablesProxy &variables, vector<Tuple> &mutexes){
-        //get all domains and id's of the variables
-        vector<vector<int>> domains;
-        vector<int> variable_id;
-        for(VariableProxy v : variables) {
-            vector<int> dom;
-            for (int d = 0; d < v.get_domain_size(); d++){
-                dom.push_back(d);
-            }
-            domains.push_back(dom);
-            variable_id.push_back(v.get_id());
-        }
-        Tuple mp = get_mutex_with_state(state, mutexes);
-
-        //while state changes check for single fact disambiguations and assign them
-        bool changed = true;
-        while (changed) {
-            changed = false;
-            for(size_t v = 0; v < state.size(); v++) {
-                if (!state.is_defined(v)){
-                    //algorithm 1 l. 4 (D_V <- F_V \ M_p)
-                    domains[v] = set_minus(domains[v], mp, variable_id[v]);
-
-                    //algorithm 1 l. 6
-                    if (domains[v].size() == 0) {
-                        return false;
-                    }
-
-                    //algorithm 1 l. 5
-                    if (domains[v].size() == 1){
-                        get_mutex_with_fact(variable_id[v], domains[v][0], mutexes, mp);
-                        changed = true;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    */
-
-    /**
      * Smaller all the domains of all variables by pruning unreachable facts
      * @param state the state of interest as vector of facts (pairs<int, int>)
      * @param variables all variables of this task  TODO: it could also just get all domains as vector<vector<int>>?
@@ -203,7 +166,7 @@ namespace potentials {
         vector<vector<int>> domains;
         for (VariableProxy v : variables) {
             vector<int> dom;
-            // For already assigned values add only this fact, for the rest the whole domain TODO: is this correct?
+            // For already assigned values add only this fact, for the rest the whole domain
             auto i = state.find(v.get_id());
             if (i != state.end()) {
                 dom.push_back(i->second);
@@ -214,6 +177,7 @@ namespace potentials {
             }
             domains.push_back(dom);
         }
+
         // A <- M_p
         Tuple a = get_mutex_with_state(state, mutexes);
 
@@ -222,17 +186,18 @@ namespace potentials {
         while (changed) {
             changed = false;
             for (VariableProxy v : variables) {
-                if (state.find(v.get_id()) == state.end()) {    // Variable is not assigned
-                    auto size = domains[v.get_id()].size();
+                int id = v.get_id();
+                if (state.find(id) == state.end()) {    // Variable is not assigned
+                    auto size = domains[id].size();
                     //algorithm 2 l. 7 (D_V <- D_V \ a)
-                    domains[v.get_id()] = set_minus(domains[v.get_id()], a, v.get_id());
+                    domains[id] = set_minus(domains[id], a, id);
 
                     //algorithm 2 l. 8 (A <- A U [intersection over all facts in D_V of M_(p U {f})] )
-                    if (domains[v.get_id()].size() < size) {
+                    if (domains[id].size() < size) {
                         vector<FactPair> mf, mf2;
-                        get_mutex_with_fact(v.get_id(), domains[v.get_id()][0], mutexes, mf);
-                        for (size_t f = 1; f < domains[v.get_id()].size(); f++) {
-                            get_mutex_with_fact(v.get_id(), domains[v.get_id()][f], mutexes, mf2);
+                        get_mutex_with_fact(id, domains[id][0], mutexes, mf);
+                        for (size_t f = 1; f < domains[id].size(); f++) {
+                            get_mutex_with_fact(id, domains[id][f], mutexes, mf2);
                             mf = intersection(mf, mf2); // Get all facts which are not part of any reachable state
                             if (mf.empty()) break;
                             mf2.clear();
@@ -264,7 +229,9 @@ namespace potentials {
         for (const map<int, int> &e : states) {
             mult = 1;
             vector<vector<int>> domains = multi_fact_disambiguation(e, variables,
-                                                                    mutexes); // This probably does some redundant work, would it be useful to give more narrowed down domains?
+                                                mutexes); // This probably does some redundant work, would it be useful to give more narrowed down domains?
+
+
             for (const vector<int> &d : domains) {
                 mult *= d.size();
                 if (mult == 0) break;
@@ -299,10 +266,10 @@ namespace potentials {
                 assigned_variables[i] = d;                      // add this fact
                 domains = multi_fact_disambiguation(assigned_variables, variables,
                                                     mutexes);// get all non-mutex domains
-                states = get_all_extensions(assigned_variables, k,
-                                            domains);           // get all extended states for this fact
-                w =  c_k_f(states, variables,
-                                  mutexes);                  // Get the non-normalized weight of this fact ... TODO: hand over domains instead of variables?))
+                states = get_all_extensions(assigned_variables, k - 1,
+                                            domains);        // get all extended states for this fact (k - 1, as one variables is assigned)
+                w = c_k_f(states, variables,
+                          mutexes);                  // Get the non-normalized weight of this fact ... TODO: hand over domains instead of variables?
                 states.clear();
                 weights_f.emplace_back(i, d, w);                // and add it to the list.
                 sum += w;
@@ -373,7 +340,6 @@ namespace potentials {
             }
             table.close();
         }
-
         int k = 1; //TODO: get from options
         vector<Weight> weights = opt_k_m(k, variables, mutexes);
         optimizer.optimize_for_weighted_samples(weights);
